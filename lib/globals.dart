79527bfcc0b9c6619fddaca5e4ae2c6757ffce2a
e <e@e.ee>
2026-08-07 13:08:43 +0300

    Version bump to 0.1.260807+3: build

--- lib/globals.dart
+++ lib/globals.dart
@@ -18,7 +18,7 @@ final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 
 const String prgName = 'easysend';
 const String progVersion = '0.1.260807';
-const int buildNumber = 2;
+const int buildNumber = 3;
 const String progAuthor = 'Eugen';
 
 const String langFile = 'assets/locales.json';
